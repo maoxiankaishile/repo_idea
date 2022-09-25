dd55fb8319b579301c2709ef16db7adfbaccd993
maoxiankaishile <105802278+maoxiankaishile@users.noreply.github.com>
2022-09-25 22:42:31 +0800

    Update UserMapper.java

--- lagou_edu_home_parent/ssm_dao/src/main/java/com/xiao/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm_dao/src/main/java/com/xiao/dao/UserMapper.java
@@ -52,5 +52,12 @@ public interface UserMapper {
      */
     public List<Resource> findResourceByRoleId(List<Integer> id);
 
+    
+     public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
 }
 
